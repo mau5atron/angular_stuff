a25dc8044ea1aff494c93a0f76d358d0ad1b287c
mau5atron <gabrielbetancourt96@outlook.com>
2018-06-03 15:31:33 -0400

    updated heroes template to show list of mock heroes - imported heroes to heros component

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,7 +12,7 @@ export class HeroesComponent implements OnInit {
 		id: 1,
 		name: "Windstorm"
 	};
-
+	heroes = HEROES;
   constructor() { }
 
   ngOnInit() {
