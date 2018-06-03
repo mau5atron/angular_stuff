182d53f35989107cf24328de4d466ca13eec7538
mau5atron <gabrielbetancourt96@outlook.com>
2018-06-03 15:10:11 -0400

    added form to hero template - added form module

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // NgModel lives in the forms module
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+
+// When the heroes component was generated, it was automatically added to the app module 
+import { HeroesComponent } from './heroes/heroes.component'; 
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { HeroesComponent } from './heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
