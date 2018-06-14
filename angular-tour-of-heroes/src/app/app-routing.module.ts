b0cce78c843b56c5d18909a036b0d336e6ddaac3
mau5atron <gabrielbetancourt96@outlook.com>
2018-06-13 20:47:38 -0400

    exorting router module for AppModule component to use it

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
-})
+  // imports: [
+  //   CommonModule
+  // ],
+  // declarations: []
+	// no need for this ^ since there is no need to declare components in a routing module
+	exports: [ RouterModule ]
+}) 
+
 export class AppRoutingModule { }
