83e811abe27dc10ee1c2e5ee2f0b175044836cff
mau5atron <gabrielbetancourt96@outlook.com>
2018-06-18 23:04:23 -0400

    imported http stuff into main app module

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -12,6 +12,10 @@ import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component'; 
 import { HttpClientModule } from '@angular/common/http';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+
 @NgModule({
   declarations: [
     AppComponent,
